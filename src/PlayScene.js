50ab7b6a577b4773a0a6987b9b3dd9ce0db1012b
Mateusz Matowicki <matowicki@gmail.com>
2021-06-17 12:38:44 +0200

    Add ground and dino to Playscene

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -7,10 +7,20 @@ class PlayScene extends Phaser.Scene {
   }
 
   create() {
+    this.gameSpeed = 10;
+    const { height, width } = this.game.config;
+
+    this.ground = this.add.tileSprite(0, height, width, 26, 'ground').setOrigin(0, 1);
+    this.dino = this.physics.add.sprite(0, height, 'dino-idle').setOrigin(0, 1)
+      .setCollideWorldBounds(true)
+      .setGravityY(4000);
+
+    this.handleInputs();
 
   }
 
   update() {
+    this.ground.tilePositionX += this.gameSpeed;
 
   }
 }
